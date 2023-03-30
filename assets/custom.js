f4ab9bc18635bc15260649a1e82a2e0a4607754b
Eric Price <info@ericprice.info>
2023-03-30 15:54:56 +0000

    add text position below, fix footer

--- assets/custom.js
+++ assets/custom.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $('body').prepend('<div class="firefighter" />');
-  $('body').prepend('<div class="squirrel" />');
-  $('body').prepend('<div class="karaoke"><span class="karaoke-active">This</span> is a Shop song</div>');
-});
+//$(document).ready(function() {
+//  $('body').prepend('<div class="firefighter" />');
+//  $('body').prepend('<div class="squirrel" />');
+//  $('body').prepend('<div class="karaoke"><span class="karaoke-active">This</span> is a Shop song</div>');
+//});
