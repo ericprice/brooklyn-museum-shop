ee5665e31530556ddeb080d88fcfe06bc3751369
Eric Price <info@ericprice.info>
2023-03-30 17:47:33 +0000

    misc

--- assets/custom.js
+++ assets/custom.js
@@ -1,5 +1,14 @@
-//$(document).ready(function() {
+$(document).ready(function() {
 //  $('body').prepend('<div class="firefighter" />');
 //  $('body').prepend('<div class="squirrel" />');
 //  $('body').prepend('<div class="karaoke"><span class="karaoke-active">This</span> is a Shop song</div>');
-//});
+
+  //$('.marquee').marquee({
+  //  speed: 1000,
+  //  gap: 50,
+  //  delayBeforeStart: 0,
+  //  direction: 'left',
+  //  duplicated: true
+  //});
+  Marquee3k.init();
+});
